b5f00e2871ffa02ada3f6602b1b91b8c2cf9e653
jm2531972 <jay.mcdonald003@gmail.com>
2015-02-08 16:17:35 -0800

    Signed-off-by: jm2531972 <jay.mcdonald003@gmail.com>

---
+++ Homework/Homework Chapter 7/Savitch_9thEd_Chap7_Prob1/main.cpp
@@ -0,0 +1,53 @@
+/* 
+ * File:   Chap 7 Savitch
+ * Author: jaymc_000
+ *
+ * Created on February 8, 2015, 3:40 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+bool firstLast2 (int [],int);
+
+int main(int argc, char** argv) {
+    //Declare Variables
+    bool isit2;
+    int count=1;
+    int count2=0;
+    int arySize=0;
+    int usrAray[100];
+    cout<<"I can find if your 2 is at the beginning or end.\n";
+    cout<<"I will soon have you input an array of integers, and then\n"
+            "find if the 2 is at the beginning or the end.\n"
+            "Please tell me how many total integers you're inputting: ";
+    cin>>arySize;
+    cout<<"Now please input your array's integers.\n";
+    for(int i=arySize;i>0;i--){
+        cout<<"Integer "<<count<<": ";
+        cin>>usrAray[count2];
+        count++;
+        count2++;
+    }
+    isit2=firstLast2(usrAray,arySize);
+    if(isit2==true){
+        cout<<"Either the first or last digit is 2.\n";
+    }
+    else if(isit2==false){
+        cout<<"Neither the first nor last digit are 2.\n";
+    }
+    return 0;
+}
+
+bool firstLast2 (int a[],int b){
+    if(a[0]==2){
+        return true;
+    }
+    if(a[b-1]==2){
+        return true;
+    }
+    else if(a[0]!=2||a[b-1]!=2){
+        return false;
+    }
+}
--- Homework/Homework Chapter 7/Savitch_9thEd_Chap7_Prob1/nbproject/Makefile-impl.mk
+++ Homework/Homework Chapter 7/Savitch_9thEd_Chap7_Prob1/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Project 2
+PROJECTNAME=Savitch_9thEd_Chap7_Prob1
 
 # Active Configuration
 DEFAULTCONF=Debug
